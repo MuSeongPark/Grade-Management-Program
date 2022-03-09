255695666614023930afe2e995552f0bfecc50bc
MuSeongPark <pms3620@gmail.com>
2022-03-09 20:26:00 +0900

    menu Manager modified

--- src/grade_management_pro/MenuManager.java
+++ src/grade_management_pro/MenuManager.java
@@ -4,10 +4,10 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
+		System.out.println("1. Add Subject");
+		System.out.println("2. Delete Subject");
+		System.out.println("3. Check my Grade");
+		System.out.println("4. Select Semester");
 		System.out.println("5. Exit");
 
 	}
