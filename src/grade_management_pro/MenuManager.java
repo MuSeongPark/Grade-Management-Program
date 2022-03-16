d3a45ac8b86a528b34ebe77c892fc61d417551b4
MuSeongPark <pms3620@gmail.com>
2022-03-16 21:32:04 +0900

    MenuManager modified(loop) ver1.1

--- src/grade_management_pro/MenuManager.java
+++ src/grade_management_pro/MenuManager.java
@@ -7,27 +7,85 @@ public class MenuManager {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
+		show_menu();
 		
-		while (true) {
-			System.out.println("Hi! this is Grade-Management-Program!");
-			System.out.println("Select number!");
+		while (true){
 			
-			System.out.println("1. Add Subject");
-			System.out.println("2. Delete Subject");
-			System.out.println("3. Check my Grade");
-			System.out.println("4. Select Semester");
-			System.out.println("5. Exit");
-			
-
 			int num = input.nextInt();
-			if (num == 5) {
+			if (num == 6) {
 				System.out.println("Grade-Management-Program is terminated");
 				break;
 			}
 			
+			switch (num) {
+				case 1:
+					add_subject();
+					break;
+				case 2:
+					delete_subject();
+					break;
+				case 3:
+					check_grade();
+					break;
+				case 4:
+					select_semester();
+					break;
+				case 5:
+					show_menu();
+					break;
+					
+			}
+			
+			
 		}
-		input.close();
+		
+
 
 	}
+	public static void add_subject() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the subject name: ");
+		String name = input.next();
+		
+		System.out.println("If you want to look Menu, Press number 5");
+	}
+	
+	public static void delete_subject() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the subject name: ");
+		String name = input.next();
+		
+		System.out.println("If you want to look Menu, Press number 5");
+	}
+	
+	public static void check_grade() {
+		System.out.println("Your total grade is");
+		//학점의 총합에 대해 보여줄 공간
+		System.out.println("If you want to look Menu, Press number 5");
+
+	}
+	
+	public static void select_semester() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the semester: ");
+		System.out.printf("Select one of the semesters (winter/first/summer/second)\n");
+		String semester = input.next();
+		//해당 학기에 대한 학점을 보여줄 공간
+		System.out.println("If you want to look Menu, Press number 5");
+	}
+	
+	public static void show_menu() {
+		System.out.println("Hi! this is Grade-Management-Program!");
+		System.out.println("Select number!");
+		
+		System.out.println("1. Add Subject");
+		System.out.println("2. Delete Subject");
+		System.out.println("3. Check my Grade");
+		System.out.println("4. Select Semester");
+		System.out.println("5. Show Menu");
+		System.out.println("6. Exit");
+		
+	}
+	
 
 }
