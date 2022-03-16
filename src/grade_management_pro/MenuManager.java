38662b4884e19b0b2f2fcde1ce943b27e746247b
MuSeongPark <pms3620@gmail.com>
2022-03-16 20:00:54 +0900

    MenuManager is modified(loop)

--- src/grade_management_pro/MenuManager.java
+++ src/grade_management_pro/MenuManager.java
@@ -1,14 +1,32 @@
 package grade_management_pro;
 
+import java.util.Scanner;
+
 public class MenuManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1. Add Subject");
-		System.out.println("2. Delete Subject");
-		System.out.println("3. Check my Grade");
-		System.out.println("4. Select Semester");
-		System.out.println("5. Exit");
+		Scanner input = new Scanner(System.in);
+		
+		while (true) {
+			System.out.println("Hi! this is Grade-Management-Program!");
+			System.out.println("Select number!");
+			
+			System.out.println("1. Add Subject");
+			System.out.println("2. Delete Subject");
+			System.out.println("3. Check my Grade");
+			System.out.println("4. Select Semester");
+			System.out.println("5. Exit");
+			
+
+			int num = input.nextInt();
+			if (num == 5) {
+				System.out.println("Grade-Management-Program is terminated");
+				break;
+			}
+			
+		}
+		input.close();
 
 	}
 
