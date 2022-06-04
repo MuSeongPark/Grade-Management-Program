bb27ac66160c59acebba7c4792ef14587cbb96d4
MuSeongPark <pms3620@gmail.com>
2022-06-04 19:05:12 +0900

    edit listener v1.0

--- src/gui/MenuSelection.java
+++ src/gui/MenuSelection.java
@@ -38,14 +38,10 @@ public class MenuSelection extends JFrame{
 		JTextArea textarea = new JTextArea();
 		cb = new JComboBox<String> (semester);
 		
-		textarea.setPreferredSize(new Dimension(400,100));
-		textarea.setEditable(false);
-
+		//레이아웃 생성
 		south_panel.setLayout(new BoxLayout(south_panel, BoxLayout.Y_AXIS));
-		//title_panel.setLayout(new BoxLayout(title_panel, BoxLayout.Y_AXIS));
 		cb_panel.setLayout(new BoxLayout(cb_panel, BoxLayout.Y_AXIS));
 		button_panel.setLayout(new GridLayout(2,2,15,15));
-		
 		
 		JLabel titlelabel = new JLabel("Welcome to Grade Manager!");
 		JLabel selectlabel = new JLabel("Select Semester");
@@ -56,19 +52,19 @@ public class MenuSelection extends JFrame{
 		JButton reset_button = new JButton("Reset");
 		
 		//setting
-		
-		
 		add_button.setPreferredSize(new Dimension(80,40));
 		calculate_button.setPreferredSize(new Dimension(80,40));
 		view_button.setPreferredSize(new Dimension(80,40));
 		reset_button.setPreferredSize(new Dimension(80,40));
+		
+		textarea.setPreferredSize(new Dimension(400,100));
+		textarea.setEditable(false);
 		
 		//listener 생성
 		AddListener addButton_listener = new AddListener(subStr);
 		CheckListener check_sub_listener = new CheckListener(subStr);
 		ResetListener reset_listener = new ResetListener();
 		CalculatorListener cal_listener = new CalculatorListener(subStr, textarea, subStr.semester);
-		
 		
 		
 		//listener 추가
@@ -78,13 +74,7 @@ public class MenuSelection extends JFrame{
 		calculate_button.addActionListener(cal_listener);
 		
 		
-		//cb = new JComboBox<String> (semester);
-		
 		//패널에 추가
-		//title_panel.add(titlelabel);
-		//title_panel.add(selectlabel);
-		//title_panel.add(cb);
-		
 		button_panel.add(add_button);
 		button_panel.add(view_button);
 		button_panel.add(calculate_button);
@@ -95,13 +85,11 @@ public class MenuSelection extends JFrame{
 		cb_panel.add(titlelabel);
 		cb_panel.add(selectlabel);
 		cb_panel.add(cb);
-		//cb_panel.add(textarea);
 		
 		south_panel.add(textarea);
 		south_panel.add(button_panel);
 		
 		
-		//this.getContentPane().add(title_panel, BorderLayout.NORTH);
 		this.getContentPane().add(cb_panel, BorderLayout.NORTH);
 		DrawImage draw = new DrawImage(this);
 		this.getContentPane().add(south_panel, BorderLayout.SOUTH);
